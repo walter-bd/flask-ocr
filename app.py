228ede8df045ec52b5c82d0305f2ece98f7a2704
wben <walterbenitez7@gmail.com>
2023-06-25 02:16:38 -0400

    Added vue/tailwind for style and modified app.py to reduce size of output

--- app.py
+++ app.py
@@ -10,23 +10,51 @@ from werkzeug.wsgi import wrap_file
 from flask import Flask, request, redirect, url_for, render_template, send_file
 from config import Config
 
+
+
 # Initialize Flask application
 app = Flask(__name__)
 app.config.from_object(Config)
 # Initialize PaddleOCR with the desired language(s)
 ocr = PaddleOCR(lang="es", use_gpu=True)     
+
+from PyPDF2 import PaperSize
+A4_w = PaperSize.A4.width
+A4_h = PaperSize.A4.height
+
+def reduce_size(pdf_bytes):
+    pdf = PdfReader(io.BytesIO(pdf_bytes))
+    pdf_writer = PdfWriter()
+    # We only have one page, so we directly access it
+    for page in pdf.pages:
+        h = float(page.mediabox.height)
+        w = float(page.mediabox.width)
+        scale_factor = min(A4_h/h, A4_w/w)
+        page.scale_by(scale_factor)
+        page.compress_content_streams()
+        pdf_writer.add_page(page)
+    pdf_writer.add_metadata(pdf.metadata)
+    with io.BytesIO() as output_file:
+        pdf_writer.write(output_file)
+        output_file.seek(0)
+        return output_file.getvalue()
+    
+
+
+
 def ocr_pdf(pdf_bytes):
     pdf = PdfReader(io.BytesIO(pdf_bytes))
     num_pages = len(pdf.pages)
     pdf_writer = PdfWriter()
     text_page = {}
     image_size = {}
-
     # Loop through each page of the PDF
     for i in range(num_pages):
         # Convert the current page to an image
         images = convert_from_bytes(pdf_bytes, first_page=i+1, last_page=i+1)
-        
+        box = pdf.pages[i].mediabox
+        height = box.height
+        width = box.width        
         # Since we're only converting one page at a time, there will only be one image in the list
         image = images[0]
 
@@ -39,7 +67,7 @@ def ocr_pdf(pdf_bytes):
         
         # Save the image to a BytesIO object
         byteIO = io.BytesIO()
-        image.save(byteIO, format='PNG')
+        image.save(byteIO, format='jpeg')
 
         # Convert the BytesIO object to a PDF
         with io.BytesIO() as f:
@@ -64,9 +92,11 @@ def ocr_pdf(pdf_bytes):
             packet.seek(0)
             text_pdf = PdfReader(packet)
             page.merge_page(text_pdf.pages[0])
+            page.scale_to(width=float(width),height=float(height))
+            page.compress_content_streams()
             pdf_writer.add_page(page)
         print(f"Page number {i} of {num_pages}")
-
+    pdf_writer.add_metadata(pdf.metadata)
     # Save the output PDF file
     with io.BytesIO() as output_file:
         pdf_writer.write(output_file)
@@ -129,6 +159,28 @@ def ocr_pdf(pdf_bytes):
 #             return output_file.getvalue()
 
 # Define a function to render the upload form
+@app.route('/resizeA4', methods=['POST'])
+def resizeA4():
+    # Check if a file was uploaded
+    if 'file' not in request.files:
+        print('No file uploaded')
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    # Check if the file is a PDF
+    if file.filename.split('.')[-1] != 'pdf':
+        print('Only PDF files are allowed')
+        return redirect(request.url)
+
+    file_contents = file.read()
+    reduced_pdf = reduce_size(file_contents)
+    buffer = io.BytesIO(reduced_pdf)
+    buffer.seek(0)
+    return send_file(buffer, as_attachment=True, download_name='reduced.pdf', mimetype="application/pdf")
+    
+    
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
